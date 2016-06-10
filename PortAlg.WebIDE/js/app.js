25e7afedf7014b5ff187e5d61659856e52ef11e1
Gabriel Weber <gabrieldsweber@gmail.com>
2016-06-09 21:28:42 -0300

    ide brand new

--- PortAlg.WebIDE/js/app.js
+++ PortAlg.WebIDE/js/app.js
@@ -1,24 +1,22 @@
-angular.module('app', [])
-.controller('editor', function ($scope) {
-    $scope.content = '';
-})
-.controller('compileController', ['$scope', '$http', function($scope, $http){
-    $scope.messagerest = 'No REST!';
-    console.log($scope.content);
-    
+angular.module('app', ['ui.ace'])
+.controller('compileController', ['$scope', '$http', function($scope, $http){    
     $scope.callCompiler = function(){
-        
+        console.log("vamo la");
         var editor = ace.edit("aceEditor");        
         var code = editor.getSession().getValue();
+        var consoleEdt = ace.edit("consoleAceEditor");
+        consoleEdt.getSession().setValue("");
         $scope.messagerest = "";
     $http.put('http://localhost:8080/PortAlg.WebService-1.0/PortAlgWS/compiler',code,
     { headers: { "Content-Type": "text/plain"} })
     .then(function successMethod(response){
-        $scope.messagerest = response.data;
+        var consoleEdt = ace.edit("consoleAceEditor");
+        consoleEdt.getSession().setValue(response.data);
         console.log(response);
     },
     function errorMethod(response){
-        $scope.messagerest = response.data;
+        var console = ace.edit("consoleAceEditor");
+        consoleEdt.getSession().setValue(response.data);
         console.log(response);
     })};
     
@@ -33,39 +31,4 @@ angular.module('app', [])
             console.log(response);
         });
     }
-}])
-.directive('ace', ['$timeout', function ($timeout) {
-
-    var resizeEditor = function (editor, elem) {
-        var lineHeight = editor.renderer.lineHeight;
-        var rows = editor.getSession().getLength();
-
-            $(elem).height("100%");
-            $(elem).width("100%");
-        editor.resize();
-    };
-
-    return {
-        restrict: 'A',
-        require: '?ngModel',
-        scope: true,
-        link: function (scope, elem, attrs, ngModel) {
-            var node = elem[0];
-
-            var editor = ace.edit(node);
-
-            editor.setTheme('ace/theme/clouds_midnight');
-
-            editor.getSession().setMode("ace/mode/csharp");
-
-            // data binding to ngModel
-            ngModel.$render = function () {
-                console.log(ngModel.$viewValue);
-                editor.setValue(ngModel.$viewValue);
-                resizeEditor(editor, elem);
-            };
-            
-            scope.$watch
-        }
-    };
 }]);
